e31a6f9a64802499af4935d1770420c795f3ef7c
Sergi Alier Vinuesa <salier@iti.es>
2023-02-27 11:53:45 +0100

    feat: add generate command

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
-import { exec } from 'child_process';
+import { exec } from "child_process";
 import { Command } from "commander";
 import inquirer from "inquirer";
 
-import { newProjectQuestions } from "./questions/new-project";
+import { newProjectQuestions, newNestElements } from "./questions/new-project";
 import { readOutput } from "./utils";
 const program = new Command();
 
-interface Answers {
+interface newAnswers {
   projectName: string;
   packageManager: string;
+}
+
+interface elmentAnswers {
+  nestElement: string;
+  elementName: string;
 }
 
 program
@@ -18,6 +23,7 @@ program
 
 program
   .command("new")
+  .alias("n")
   .description("Makes a default new project")
   /*.argument("[projectName]", "name of the project.")
   .option(
@@ -25,11 +31,29 @@ program
     "Package manager to use in the project. [npm, yarn, pnpm]"
   )*/
   .action(async () => {
-    const answers = inquirer.prompt<Answers>(newProjectQuestions).then((answers) => {
-      var p = exec("nest new -d " + answers.projectName + " -p " + answers.packageManager);
-      console.log(answers);
-      readOutput(p);
-    });
+    const answers = inquirer
+      .prompt<newAnswers>(newProjectQuestions)
+      .then((answers) => {
+        var p = exec(
+          "nest new -d " + answers.projectName + " -p " + answers.packageManager
+        );
+        console.log(answers);
+        readOutput(p);
+      });
+  });
+
+program
+  .command("generate")
+  .alias("g")
+  .description("Generates a Nest element")
+  .action(async () => {
+    const answers = inquirer
+      .prompt<elmentAnswers>(newNestElements)
+      .then((answers) => {
+        var p = exec("nest generate -d " + answers.nestElement + " " + answers.elementName);
+        console.log(answers);
+        readOutput(p);
+      });
   });
 
 program.parse();
--- src/questions/new-project.ts
+++ src/questions/new-project.ts
@@ -7,3 +7,13 @@ export const newProjectQuestions = [
     "choices": ["npm", "yarn", "pnpm"]
   }
 ];
+
+export const newNestElements = [
+  {
+    "type": "list",
+    "name": "nestElement",
+    "message": "Which Nest element do you want to generate?",
+    "choices": ["application", "class", "configuration", "controller", "decorator", "filter", "gateway", "guard", "interceptor", "interface", "middleware", "module", "pipe", "provider", "resolver", "service", "library", "sub-app", "resource"]
+  },
+  { "type": "input", "name": "elementName", "message": "What is the name for the new element?" }
+];
