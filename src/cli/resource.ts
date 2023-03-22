39d918b4f0eb139c7724d0d498ca840faa4c6897
Sergi Alier Vinuesa <salier@iti.es>
2023-03-22 14:34:34 +0100

    feat: resource has correct builder pattern

--- src/cli/resource.ts
+++ src/cli/resource.ts
@@ -4,18 +4,32 @@ import fs from "fs";
 import { newResourceQuestions } from "./questions/resource";
 import { OptionsMapping, optionsToArgs } from "./mapper";
 import { readOutput } from "../io";
+import { IsAlpha, IsBoolean, IsOptional } from "class-validator";
+import { nestNewResource } from "../resource";
+import { logger } from "../logger";
 
 export interface ElementAnswers {
   resourceName: string;
   project: string;
 }
 
-interface ProjectOptions {
-  dryRun: boolean;
+export class ProjectChoices {
+  @IsAlpha()
+  name: string;
+
+  options: ProjectOptions;
+}
+
+export class ProjectOptions {
+  @IsOptional()
+  @IsBoolean()
+  dryRun?: boolean;
+
+  @IsBoolean()
   project: string;
 }
 
-const toNestOptions: OptionsMapping<ProjectOptions> = {
+export const toNestOptions: OptionsMapping<ProjectOptions> = {
   dryRun: "-d",
   project: "-p",
 };
@@ -24,6 +38,10 @@ export const handleResourceCommand = async (
   elementName: string,
   options: ProjectOptions
 ) => {
+  const choices: ProjectChoices = {
+    name: elementName,
+    options,
+  };
   const optionString = optionsToArgs(options, toNestOptions);
   if (!elementName) {
     const answers = await inquirer.prompt<ElementAnswers>(newResourceQuestions);
@@ -31,21 +49,15 @@ export const handleResourceCommand = async (
     options.project = answers.project;
   }
 
-  if (options.dryRun) {
-    return;
-  }
-
   if (!fs.existsSync(options.project)) {
     console.log(`Project directory '${options.project}' does not exist.`);
     return;
   }
-  const originalDirectory = process.cwd();
-
-  process.chdir(options.project);
-  const outputNest = exec(`nest generate resource ${elementName}`);
-  await readOutput(outputNest);
-
-  process.chdir(originalDirectory);
-  const hygen = exec(`hygen resource new --name ${elementName} --project ${options.project}`);
-  await readOutput(hygen);
+  
+  try {
+    nestNewResource(choices);
+  } catch (err: any) {
+    logger.error(`Something was wrong ${err}`);
+    process.exitCode = 1;
+  }
 };
--- src/resource.ts
+++ src/resource.ts
@@ -1,2 +1,52 @@
-// Afegir les qüestions relacionades amb la lógica de execució i control
-// d'errors en la creació d'un nou resource
+import { ChildProcess, exec } from "child_process";
+import { readOutput } from "./io";
+import { optionsToArgs } from "./cli/mapper";
+import { ProjectOptions, ProjectChoices, toNestOptions } from "./cli/resource";
+import { logger } from "./logger";
+
+export async function nestNewResource(choices: ProjectChoices) {
+  const optionString = optionsToArgs(choices.options, toNestOptions);
+
+  const originalDirectory = process.cwd();
+  process.chdir(choices.options.project);
+  try {
+    await nestResourceGenerate(optionString, choices);
+    if (choices.options.dryRun) {
+      return;
+    }
+    process.chdir(originalDirectory);
+    await hygenDependencies(choices.options, choices);
+  } catch (err: any) {
+    logger.error(`Something was wrong ${err}`);
+    throw err;
+  }
+}
+async function nestResourceGenerate(
+  optionString: string,
+  choices: ProjectChoices
+): Promise<void> {
+  logger.info(`Creating new nest resource`);
+  await execFunction(`nest generate resource ${optionString} ${choices.name}`);
+}
+
+async function hygenDependencies(
+  options: ProjectOptions,
+  choices: ProjectChoices
+): Promise<void> {
+  logger.info(`Creating Hygen depencencies`);
+  await execFunction(`hygen resource new --name ${choices.name} --project ${options.project}`);
+}
+
+async function execFunction(functionToExecute: string): Promise<void> {
+    const funct = exec(functionToExecute);
+    await readAndCheckOutput(funct);
+}
+
+async function readAndCheckOutput(outputNest: ChildProcess) {
+  try {
+    await readOutput(outputNest);
+  } catch (err: any) {
+    logger.error(`Something was wrong ${err}`);
+    throw err;
+  }
+}
