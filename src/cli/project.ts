4dfaaeeda742ef6c4f655af0388d6cdb39c928ca
Sergi Alier Vinuesa <salier@iti.es>
2023-03-22 11:13:47 +0100

    feat: builder pattern for project generator

--- src/cli/project.ts
+++ src/cli/project.ts
@@ -1,8 +1,7 @@
 import { IsAlpha, IsBoolean, IsEnum, IsOptional } from "class-validator";
-import { exec } from "child_process";
-import { OptionsMapping, optionsToArgs } from "./mapper";
+import { OptionsMapping } from "./mapper";
 import { validate } from "./validator";
-import { readOutput } from "../io";
+import { NestJSProjectBuilder } from "../project";
 
 export enum PackageManager {
   YARN = "yarn",
@@ -29,7 +28,7 @@ export class ProjectOptions {
   skipGit?: boolean;
 }
 
-const toNestOptions: OptionsMapping<ProjectOptions> = {
+export const toNestOptions: OptionsMapping<ProjectOptions> = {
   dryRun: "-d",
   skipGit: "-s",
 };
@@ -55,33 +54,7 @@ export const handleProjectCommand = async (
     return;
   }
 
-  const optionString = optionsToArgs(choices.options, toNestOptions);
+  const builder = new NestJSProjectBuilder();
+  builder.build(choices);
 
-  const outputNest = exec(
-    `nest new ${optionString} ${choices.name} -p ${choices.packageManager}`
-  );
-  try {
-    await readOutput(outputNest);
-  } catch (err: any) {
-    console.error(`Something was wrong ${err}`);
-    return;
-  }
-
-  if (choices.options.dryRun) {
-    return;
-  }
-
-  const outputPackage = exec(
-    `hygen dependencies new --project ${choices.name} --packageManager ${choices.packageManager}`
-  );
-  await readOutput(outputPackage).catch((reason) => {
-    console.error(`Something was wrong ${reason}`);
-  });
-
-  const hygen = exec(
-    `hygen controller new --project ${choices.name} && hygen persistence new --project ${choices.name} && hygen app new --project ${choices.name}`
-  );
-  await readOutput(hygen).catch((reason) => {
-    console.error(`Something was wrong ${reason}`);
-  });
 };
--- src/project.ts
+++ src/project.ts
@@ -1,2 +1,45 @@
-// Afegir les qüestions relacionades amb la lógica de execució i control
-// d'errors en la creació d'un nou projecte
+import { exec } from "child_process";
+import { readOutput } from "./io";
+import { optionsToArgs } from "./cli/mapper";
+import { ProjectChoices, toNestOptions } from "./cli/project";
+
+export interface ProjectBuilder {
+  build(choices: ProjectChoices): Promise<void>;
+}
+
+export class NestJSProjectBuilder implements ProjectBuilder {
+  async build(choices: ProjectChoices): Promise<void> {
+
+    const optionString = optionsToArgs(choices.options, toNestOptions);
+    const outputNest = exec(
+      `nest new ${optionString} ${choices.name} -p ${choices.packageManager}`
+    );
+
+    try {
+      await readOutput(outputNest);
+    } catch (err: any) {
+      console.error(`Something was wrong ${err}`);
+      return;
+    }
+
+    if (choices.options.dryRun) {
+      return;
+    }
+
+    const outputPackage = exec(
+      `hygen dependencies new --project ${choices.name} --packageManager ${choices.packageManager}`
+    );
+
+    await readOutput(outputPackage).catch((reason) => {
+      console.error(`Something was wrong ${reason}`);
+    });
+
+    const hygen = exec(
+      `hygen controller new --project ${choices.name} && hygen persistence new --project ${choices.name} && hygen app new --project ${choices.name}`
+    );
+
+    await readOutput(hygen).catch((reason) => {
+      console.error(`Something was wrong ${reason}`);
+    });
+  }
+}
