3bc7e8c9f81efe1250c7695d1a27bc0f9d7e3b17
Sergi Alier Vinuesa <salier@iti.es>
2023-04-26 11:28:33 +0200

    feat:add apiPropertyToString  function to simplify

--- src/resource.ts
+++ src/resource.ts
@@ -13,8 +13,6 @@ export async function generateNewResource(
   choices: ResourceChoices
 ): Promise<void> {
   const optionString = optionsToArgs(choices.options, toNestOptions);
-  let entityContent = "";
-  let createDtoContent = "";
 
   const originalDirectory = process.cwd();
   process.chdir(choices.options.project);
@@ -28,21 +26,24 @@ export async function generateNewResource(
     await hygenDependencies(choices.options, choices);
     if (choices.options.json) {
       if (pathExists(choices.options.json)) {
-        entityContent += await JSONtoEntity(choices.options.json);
-        createDtoContent += await JSONtoCreate(choices.options.json, choices.name);
+        const entityContent = await JSONtoEntity(choices.options.json);
+        const createDtoContent = await JSONtoCreate(
+          choices.options.json,
+          choices.name
+        );
 
         fs.writeFileSync(
-         `./${choices.options.project}/src/${choices.name}/entities/${choices.name}.entity.ts`,
-         entityContent,
-         { flag: "a" }
+          `./${choices.options.project}/src/${choices.name}/entities/${choices.name}.entity.ts`,
+          entityContent,
+          { flag: "a" }
         );
         fs.writeFileSync(
           `./${choices.options.project}/src/${choices.name}/entities/create-${choices.name}.dto.ts`,
           createDtoContent,
           { flag: "w" }
-         );
+        );
         await execFunction(
-         `hygen entityContent add --name ${choices.name} --project ${choices.options.project}`
+          `hygen entityContent add --name ${choices.name} --project ${choices.options.project}`
         );
       } else {
         Logger.error(`File ${choices.options.json} does not exist`);
@@ -74,67 +75,64 @@ async function hygenDependencies(
 
 export async function JSONtoEntity(json: string): Promise<string> {
   let entityObject = await checkJSON(json);
-  return Object.entries(entityObject)
-    .map(([key, property]) => {
-      const sizeString = property.size ? `, length: ${property.size}` : "";
-      let defaultString = "";
-      if (property.default !== undefined) {
-        if (typeof property.default === "string") {
-          defaultString = `, default: '${property.default}'`;
-        } else {
-          defaultString = `, default: ${property.default}`;
+  return (
+    Object.entries(entityObject)
+      .map(([key, property]) => {
+        const sizeString = property.size ? `, length: ${property.size}` : "";
+        let defaultString = "";
+        if (property.default !== undefined) {
+          if (typeof property.default === "string") {
+            defaultString = `, default: '${property.default}'`;
+          } else {
+            defaultString = `, default: ${property.default}`;
+          }
         }
-      }
-      let apiPropertyString = "";
-      const apiProperty = property.annotations?.apiProperty ?? ApiProperty.UNDOCUMENTED;
-      const exclude = property.annotations?.exclude;
-      if (apiProperty === ApiProperty.REQUIRED) {
-        apiPropertyString = `@ApiProperty({ required: true })\n`;
-      } else if (apiProperty === ApiProperty.OPTIONAL) {
-        apiPropertyString = `@ApiPropertyOptional()\n`;
-      } else if (apiProperty === ApiProperty.UNDOCUMENTED) {
-        apiPropertyString = `@ApiHideProperty()\n`;
-      }
-      const excludeString = exclude ? "@Exclude()\n" : "";
-      return `${excludeString}${apiPropertyString}@Column({ type: '${property.sqlType}'${sizeString}${defaultString} })\n${key}: ${property.type};\n`;
-    })
-    .join("\n")+ "}";
+        const apiPropertyString = apiPropertyToString(property, "entity");
+        return `${apiPropertyString}@Column({ type: '${property.sqlType}'${sizeString}${defaultString} })\n${key}: ${property.type};\n`;
+      })
+      .join("\n") + "}"
+  );
 }
 
-export async function JSONtoCreate(json: string, className: string): Promise<string> {
+export async function JSONtoCreate(
+  json: string,
+  className: string
+): Promise<string> {
   let entityObject = await checkJSON(json);
   return `import { ApiHideProperty, ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class Create${className}Dto {
     ${Object.entries(entityObject)
-    .map(([key, property]) => {
-      if (property.annotations?.exclude) {
-        return "";
-      }
-        let apiPropertyString = "";
-        const apiProperty = property.annotations?.apiProperty ?? ApiProperty.UNDOCUMENTED;
-        const exclude = property.annotations?.exclude;
-        if (apiProperty === ApiProperty.REQUIRED) {
-          apiPropertyString = `@ApiProperty()\n`;
-        } else if (apiProperty === ApiProperty.OPTIONAL) {
-          apiPropertyString = `@ApiPropertyOptional()\n`;
-          key += "?";
-        } else if (apiProperty === ApiProperty.UNDOCUMENTED) {
-          apiPropertyString = `@ApiHideProperty()\n`;
-      }
-      if (property.annotations?.isNotEmpty) {
-        apiPropertyString += `@IsNotEmpty()\n`;
-      }
-      if (property.annotations?.isString) {
-        apiPropertyString += `@IsString()\n`;
-      }
-        const excludeString = exclude ? "@Exclude()\n" : "";
-        return `${excludeString}${apiPropertyString}${key}: ${property.type};\n`;
+      .map(([key, property]) => {
+        if (property.annotations?.exclude) {
+          return "";
+        }
+        let apiPropertyString = apiPropertyToString(property, "create");
+        return `${apiPropertyString}${key}: ${property.type};\n`;
       })
       .join("\n")}
 }`;
+}
+
+function apiPropertyToString(property: any, type: string): string {
+  const apiProperty =
+    property.annotations?.apiProperty ?? ApiProperty.UNDOCUMENTED;
+  let apiPropertyString = property.annotations?.exclude ? "@Exclude()\n" : "";
+  if (apiProperty === ApiProperty.REQUIRED) {
+    apiPropertyString = `@ApiProperty({ required: true })\n`;
+  } else if (apiProperty === ApiProperty.OPTIONAL) {
+    apiPropertyString = `@ApiPropertyOptional()\n`;
+  } else if (apiProperty === ApiProperty.UNDOCUMENTED) {
+    apiPropertyString = `@ApiHideProperty()\n`;
   }
-
-
-
+  if (type === "create") {
+    if (property.annotations?.isNotEmpty) {
+      apiPropertyString += `@IsNotEmpty()\n`;
+    }
+    if (property.annotations?.isString) {
+      apiPropertyString += `@IsString()\n`;
+    }
+  }
+  return apiPropertyString;
+}
